2c9d1020deaf1db46e3b778773b94a7bc29af63f
Cassiokmargo <cassio.dev.camargo@gmail.com>
2026-02-20 15:37:03 -0400

    chore: add shadcn, prettier and  eslint setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div>
         <Button>BOTAO</Button>
+        <Input placeholder="Campo teste" />
       </div>
     </div>
   );
